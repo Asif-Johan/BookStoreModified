6b59a266fa8b78c171e3ba5f6ae1f2a07780abcf
Asif Johan <jjohan357@gmail.com>
2024-03-06 14:48:01 +0600

    5th commit Router dom added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import {Routes, Route} from 'react'
+
+//import from pages
+import Home from './pages/home';
+import createBooks from './pages/createBooks';
+import deleteBooks from './pages/deleteBook';
+import editBook from './pages/editBook';
+import showBook from './pages/showBook';
+
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>
-      App
-    </div>
+    <Routes>
+      <Route path='/' element={<Home></Home>}/>
+      <Route path='/books/create' element={<createBooks></createBooks>}/>
+      <Route path='/books/details/:id' element={<showBook></showBook>}/>
+      <Route path='/book/edit/:id' element={<editBook></editBook>}/>
+      <Route path='/book/delete/:id' element={<deleteBooks></deleteBooks>}/>
+    </Routes>
   )
 }
 
---
+++ frontend/src/pages/createBooks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const createBooks = () => {
+  return (
+    <div>
+      Create Books
+    </div>
+  )
+}
+
+export default createBooks
